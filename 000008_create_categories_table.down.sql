cc31d6e0ba57274d05173e3151157898ff0a66eb
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-03 13:52:53 +0700

    delete: duplicate file migration

--- 000008_create_categories_table.down.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "categories" (
-    "id" serial PRIMARY KEY,
-    "name" varchar(100) UNIQUE NOT NULL,
-    "created_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "updated_at" timestamp DEFAULT(CURRENT_TIMESTAMP),
-    "created_by" int,
-    "updated_by" int
-);
